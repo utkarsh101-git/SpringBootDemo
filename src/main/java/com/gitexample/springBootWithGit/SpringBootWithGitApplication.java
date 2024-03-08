95fb7322a3a1e671a89abbf0822bf46733a979fc
Utkarsh <misrautkarsh72@gmail.com>
2024-03-08 17:35:37 +0530

    Project Setup with dependencies and database connections

---
+++ src/main/java/com/gitexample/springBootWithGit/SpringBootWithGitApplication.java
@@ -0,0 +1,13 @@
+package com.gitexample.springBootWithGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitexample/springBootWithGit/SpringBootWithGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitexample.springBootWithGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
